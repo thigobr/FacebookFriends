45343aa18fe6f21ea11fb2562d855cf3758144dd
Thiago Rocha <thiago@basillabs.co>
2014-07-06 21:23:04 -0300

    Lazy load list

--- src/com/blue/facebookfriends/FriendListAdapter.java
+++ src/com/blue/facebookfriends/FriendListAdapter.java
@@ -1,9 +1,12 @@
 package com.blue.facebookfriends;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import android.content.Context;
 import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,30 +14,49 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Response.PagingDirection;
+import com.facebook.Session;
+import com.facebook.model.GraphMultiResult;
+import com.facebook.model.GraphObject;
+import com.facebook.model.GraphObjectList;
 import com.facebook.model.GraphUser;
 import com.squareup.picasso.Picasso;
 
 public class FriendListAdapter extends ArrayAdapter<GraphUser> {
+    protected static final String TAG = "FriendListAdapter";
+    private static final int sBatchSize = 10;
     private final Context mContext;
     private final List<GraphUser> mFriends;
     private final int mAvatarSize;
+    private Request mNextRequest;
+    private View mLoadingView;
+    private Boolean mIsLoading;
 
-    public FriendListAdapter(Context context, int resource,
-            List<GraphUser> objects) {
-        super(context, resource, objects);
+    public FriendListAdapter(Context context, int resource, Session session) {
+        super(context, resource);
 
         this.mContext = context;
-        this.mFriends = objects;
         this.mAvatarSize = (int) context.getResources().getDimension(
                 R.dimen.avatar_size);
+
+        mFriends = new ArrayList<GraphUser>();
+        if (session != null && session.isOpened()) {
+            executeFirstQueryAsync(session);
+        }
+
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mLoadingView = inflater.inflate(R.layout.loading_item, null, false);
     }
 
     @Override
     public int getCount() {
-        if (mFriends == null) {
-            return 0;
+        if (mLoadingView != null) {
+            return mFriends.size() + 1;
+        } else {
+            return mFriends.size();
         }
-        return mFriends.size();
     }
 
     static class ViewHolder {
@@ -43,9 +65,17 @@ public class FriendListAdapter extends ArrayAdapter<GraphUser> {
     }
 
     public View getView(int position, View convertview, ViewGroup parent) {
+        if (position >= mFriends.size()) {
+            if (!mIsLoading  && mNextRequest != null) {
+                mIsLoading = true;
+                mNextRequest.executeAsync();
+            }
+            return mLoadingView;
+        }
+
         ViewHolder viewHolder;
 
-        if (convertview == null) {
+        if (convertview == null || convertview.getTag() == null) {
             LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertview = inflater.inflate(R.layout.list_item, null, false);
             viewHolder = new ViewHolder();
@@ -65,7 +95,7 @@ public class FriendListAdapter extends ArrayAdapter<GraphUser> {
             .authority("graph.facebook.com")
             .appendPath(friend.getId().toString())
             .appendPath("picture")
-            .appendQueryParameter("type", "medium");
+            .appendQueryParameter("type", "large");
 
         Picasso.with(mContext).setLoggingEnabled(true);
         Picasso.with(mContext).load(builder.build())
@@ -75,5 +105,34 @@ public class FriendListAdapter extends ArrayAdapter<GraphUser> {
             .into(viewHolder.pictureHolder);
 
         return convertview;
+    }
+
+    private final Request.Callback mCallBack = new Request.Callback() {
+        @Override
+        public void onCompleted(Response response) {
+            Log.d(TAG, "Friend request completed!");
+            mNextRequest = response.getRequestForPagedResults(PagingDirection.NEXT);
+            if (mNextRequest != null) {
+                mNextRequest.setCallback(mCallBack);
+            } else {
+                mLoadingView = null;
+            }
+            updateDataSet(response);
+        }
+    };
+
+    private void updateDataSet(Response response) {
+        GraphMultiResult multiResult = response.getGraphObjectAs(GraphMultiResult.class);
+        GraphObjectList<GraphObject> data = multiResult.getData();
+        mFriends.addAll(data.castToListOf(GraphUser.class));
+        mIsLoading = false;
+        notifyDataSetChanged();
+    }
+
+    private void executeFirstQueryAsync(Session session) {
+        Bundle b = new Bundle();
+        b.putInt("limit", sBatchSize);
+        mIsLoading = true;
+        new Request(session, "me/friends", b, null, mCallBack).executeAsync();
     }
 }
--- src/com/blue/facebookfriends/FriendListFragment.java
+++ src/com/blue/facebookfriends/FriendListFragment.java
@@ -1,16 +1,10 @@
 package com.blue.facebookfriends;
 
-import java.util.List;
-
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
 
-import com.facebook.Request;
-import com.facebook.Request.GraphUserListCallback;
-import com.facebook.Response;
 import com.facebook.Session;
-import com.facebook.model.GraphUser;
 
 public class FriendListFragment extends ListFragment {
     private static String TAG = "FriendListFragment";
@@ -22,13 +16,7 @@ public class FriendListFragment extends ListFragment {
         Log.d(TAG, "Friend Fragment Called");
 
         Session session = Session.getActiveSession();
-
-        Request.newMyFriendsRequest(session, new GraphUserListCallback() {
-            @Override
-            public void onCompleted(List<GraphUser> users, Response response) {
-                mListAdapter = new FriendListAdapter(getActivity(), 0, users);
-                FriendListFragment.this.setListAdapter(mListAdapter);
-            }
-        }).executeAsync();
+        mListAdapter = new FriendListAdapter(getActivity(), 0, session);
+        setListAdapter(mListAdapter);
     }
 }
